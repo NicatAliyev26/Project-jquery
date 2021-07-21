2ca14f0aef8eeb2d1d747c84956f9fb771e85825
Nicat Aliyev <67416106+NicatAliyev26@users.noreply.github.com>
2021-07-21 16:05:12 +0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,54 @@
+ $(document).ready(function(){
+  $("#btn").click(function(){
+    $("#per-show").animate({
+        width:'490px'
+    });
+  });
+});
+$(document).ready(function(){
+  $("#btn").click(function(){
+    $("#p").show("slow");
+  });
+});
+// .................................
+   $(document).ready(function(){
+  $("#btn1").click(function(){
+    $("#per-show1").animate({
+        width:'450px'
+    });
+  });
+});
+   
+   $(document).ready(function(){
+  $("#btn1").click(function(){
+    $("#p1").show("slow");
+  });
+});
+// ...................................
+$(document).ready(function(){
+  $("#btn2").click(function(){
+    $("#per-show2").animate({
+        width:'250px'
+    });
+  });
+});
+   
+   $(document).ready(function(){
+  $("#btn2").click(function(){
+    $("#p2").show("slow");
+  });
+});
+// ....................................
+$(document).ready(function(){
+  $("#btn3").click(function(){
+    $("#per-show3").animate({
+        width:'360px'
+    });
+  });
+});
+   
+   $(document).ready(function(){
+  $("#btn3").click(function(){
+    $("#p3").show("slow");
+  });
+});
